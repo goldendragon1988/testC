98380acd62c52e68e1899d39e7ee9c8762c77abd
jim <jag.arnold.go@gmail.com>
2020-09-06 18:30:35 +0800

    add test helper and user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  # ensure User model has a 1:m relationship with the Item model
+  it { should have_many(:providers).dependent(:destroy) }
+  # Validation tests
+  # ensure columns title and created_by are present before saving
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
 end
